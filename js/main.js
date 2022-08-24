52ba2c6658ea208af075ee6eb6c91f5b29cd2aea
ᗰᖇ.ᑕᗩᖇᒪ <93263155+PulCarl@users.noreply.github.com>
2022-08-24 14:13:55 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('playButton').onclick = function(e) {
-   move('.Acceuil').translate(-450, -250).end();
-   move('.Competence').ease('in').translate(-450, -250).end();
-   move('.Projet').ease('in').translate(-250, -450).end();
-   move('.Contact').ease('in').translate(150, -450).end();
-
-};
